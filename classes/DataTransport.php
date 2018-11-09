6456f2f05049033ec6697d30289b0955b0ba038c
MesClics <mesclics@hotmail.com>
2018-11-09 17:52:41 +0100

    add files attribute to DataTransport and its hydratation in constructor

--- classes/DataTransport.php
+++ classes/DataTransport.php
@@ -1,12 +1,18 @@
 <?php
 class DataTransport{
     private $post;
+    private $files;
 
-    public function __construct(Array $post){
+    public function __construct(Array $post, Array $files = null){
         //securiser tous les éléments saisis par le visiteur
         foreach($post as $k => $v){
             $post[$k] = htmlspecialchars(stripslashes($v));
         }
         $this->post = $post;
+
+        if($files){
+            // TODO: ajouter des étapes de sécurisations ici
+            $this->files = $files;
+        }
     }
 }
