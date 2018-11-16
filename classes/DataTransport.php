e4a11ad88d1e5ca4d631c2ba0d877b9c7ea2312a
MesClics <mesclics@hotmail.com>
2018-11-16 16:30:16 +0100

    add array treatment for security in constructor

--- classes/DataTransport.php
+++ classes/DataTransport.php
@@ -7,12 +7,17 @@ class DataTransport{
     public function __construct(Array $post, Array $files = null){
         //securiser tous les éléments saisis par le visiteur
         foreach($post as $k => $v){
-            $post[$k] = htmlspecialchars(stripslashes($v));
+            if(is_string($v)){
+                $post[$k] = htmlspecialchars(stripslashes($v));
+            }
+            if(is_array($v)){
+                array_walk_recursive($v, "self::secureArray");
+            }
         }
         $this->post = $post;
 
         if($files){
-            // TODO: ajouter des étapes de sécurisations ici
+            // TODO: ajouter des étapes de sécurisation ici
             $this->files = $files;
         }
     }
@@ -23,5 +28,15 @@ class DataTransport{
 
     public function getPost(){
         return $this->post;
+    }
+
+    protected function secureArray(&$item, $key){
+        if(is_string($item)){
+            $item = htmlspecialchars($item);
+        }
+
+        if(is_array($item)){
+            array_walk_recursive($item, "self::secureArray");
+        }
     }
 }
